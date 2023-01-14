1aff1cb97d6d69d73ac5b90dbcabc5b331537ea7
Enock <enock.mokua@student.moringaschool.com>
2023-01-15 00:39:59 +0300

    add default 0

--- db/migrate/20230114212930_add_role_to_user.rb
+++ db/migrate/20230114212930_add_role_to_user.rb
@@ -1,5 +1,6 @@
 class AddRoleToUser < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :role, :integer
+    add_column :users, :role, :integer, default: 0
+    #Ex:- :default =>''
   end
 end
